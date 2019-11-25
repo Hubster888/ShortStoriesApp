5a029e76714f3208c4759541e39858846d055ca9
Hubert <hubertrzeminski16@gmail.com>
2019-11-25 08:58:52 +0000

    Initial Commit

---
+++ ShortStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShortStories
+//
+//  Created by Hubert Rzeminski on 25/11/2019.
+//  Copyright © 2019 Hubert Rzeminski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
